59c5f9a1069baa81ddde46dcf7ef404b8a8531ed
Brian Ngo <b_ngo13@u.pacific.edu>
2025-02-06 19:46:01 -0800

    Finished base HW2

--- HW2/HW2.m
+++ HW2/HW2.m
@@ -3,22 +3,49 @@ function [] = HW2()
     load ECG_1.mat ECG Fs;
 
     for i = 1:12
-        % Plot for each patient
-        fprintf("Patient %d\n", i);
+        %% Init data for current sample %%
+        fprintf("Sample %d\n", i);
         patient_ECG = ECG(i, :);
-        plot(patient_ECG)
-        
-        % Calculate heart rate of patient every minute
+        data_len = size(patient_ECG, 2);
+
+        %% Sample the data in seconds %%
+        fprintf("Displaying data in seconds...\n")
+        patient_ECG_y_sec = 0 : (1/Fs) : fix(data_len/Fs);
+        plot(patient_ECG_y_sec, patient_ECG);
+        fprintf("Press any key to continue...\n")
+        pause
+
+        %% Get and sample data in minutes
         samples_per_min = Fs * 60;
         current_sample = 1;
-
-        while current_sample < 96000
-            patient_hr_minute = patient_ECG(current_sample : current_sample + samples_per_min);
+        patient_ECG_min = [];
+        
+        j = 1;
+        % Calculate the HR of a patient every minute
+        while current_sample < data_len 
+            patient_hr_minute = patient_ECG(current_sample : (current_sample + samples_per_min));
             hr = getheartbeat(patient_hr_minute);
+            patient_ECG_min(j) = hr;
             fprintf("Patient heartrate at %d minute(s): %d\n", fix(current_sample / samples_per_min) + 1, hr);
             current_sample = current_sample + samples_per_min;
+            j = j + 1;
         end
 
+        % Display the data
+        patient_ECG_y_min = 1 : fix(data_len/samples_per_min);
+        plot(patient_ECG_y_min, patient_ECG_min);
+        fprintf("Displaying patient heart rate in minutes...\n")
+        fprintf("Press any key to continue...\n")
+        pause
+
+        %% Calculate hr variability per min
+
+        patient_ECG_min_variability = [];
+        for j = 2:size(patient_ECG_min, 2)
+            patient_ECG_min_variability(j - 1) = abs(patient_ECG_min(j - 1) - patient_ECG_min(j));
+        end
+        plot(patient_ECG_min_variability);
+        fprintf("Displaying heart rate variability in minutes...\n")
         fprintf("Press any key to continue...\n")
         pause
     end
--- HW2/getheartbeat.m
+++ HW2/getheartbeat.m
@@ -1,4 +1,4 @@
 function heartbeat = getheartbeat(data)
-    heartbeats = findpeaks(data, "MinPeakProminence", 1);
+    heartbeats = findpeaks(data, "MinPeakProminence", 0.5);
     heartbeat = size(heartbeats, 2);
 end
