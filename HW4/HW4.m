ebe0edfab982ec2f837ee5d9f98acf104f07126e
Brian Ngo <b_ngo13@u.pacific.edu>
2025-04-08 17:28:55 -0700

    Smoothed LowPass ECG

--- HW4/HW4.m
+++ HW4/HW4.m
@@ -3,10 +3,12 @@ function HW4
     load ECG_1.mat ECG Fs
     ECG_data = ECG(1, :);
     viewWindow = Fs * 10;
+    peakVal = 0.1;
 
     % ECG
     subplot(6, 1, 1);
-    plot(ECG_data(1:viewWindow))
+    plot(ECG_data)
+    xlim([1 viewWindow])
     title("Original ECG")
 
     % BP Filter
@@ -14,28 +16,39 @@ function HW4
     b = fir1(42, fValues, "bandpass");
     bp_ECG = filter(b, 1, ECG_data);
     subplot(6, 1, 2);
-    plot(bp_ECG(1:viewWindow))
+    plot(bp_ECG)
+    xlim([1 viewWindow])
     title("BandPass ECG")
 
     % D/DT
     diff_ECG = diff(bp_ECG);
     subplot(6, 1, 3);
-    plot(diff_ECG(1:viewWindow))
+    plot(diff_ECG)
+    xlim([1 viewWindow])
     title("D/DT ECG")
     
     % ||
     abs_ECG = abs(diff_ECG);
     subplot(6, 1, 4);
-    plot(abs_ECG(1:viewWindow))
+    plot(abs_ECG)
+    xlim([1 viewWindow])
     title("ABS ECG")
 
     % LP Filter
     lpVal = 150 / Fs;
-    b = fir1(48, lpVal, "low");
+    b = fir1(48, lpVal, "low", chebwin(49, 30));
     lp_ECG = filter(b, 1, abs_ECG);
+    lp_ECG = movmean(lp_ECG, 8);
     subplot(6, 1, 5);
-    plot(lp_ECG(1:viewWindow))
+    plot(lp_ECG)
+    xlim([1 viewWindow])
     title("LowPass ECG")
 
     % Threshold
+    [~, qrs] = findpeaks(lp_ECG, "MinPeakProminence", peakVal);
+    qrs = qrs / Fs;
+    rrIntervals = diff(qrs);
+    subplot(6, 1, 6);
+    histogram(rrIntervals, 25);
+    title("Average RR Interval");
 end
